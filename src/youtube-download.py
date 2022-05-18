43aba079b14f920988c0e92288167b054a30d663
abdalrzag eisa <bmjfdrh@gmail.com>
2022-05-18 08:33:57 +0300

    youtube-download

--- src/youtube-download.py
+++ src/youtube-download.py
@@ -2,21 +2,52 @@ from __future__ import unicode_literals
 import yt_dlp
 import sys
 import typing
-import os
+import argparse
 
-urls = sys.argv[1:]
-
-if len(urls) == 0:
-    print("Please provide a youtube url!")
-    os._exit(1)
+def main() -> None:
+    parser = argparse.ArgumentParser()
     
-ydl_opts: typing.Any = {
-    "outtmpl": "%(title)s.%(ext)s",
-    # "restrictfilenames": True, # replace space with _
-    "ignoreerrors": True,
-    "format": "mp4",
-    "continuedl": True
-}
+    parser.add_argument('--audio', '-a', action='store_true', help='download audio')
+    args = parser.parse_args()
+    
+    ydl_opts: typing.Dict = {}
+    
+    if args.audio:        
+        ydl_opts = {
+            "outtmpl": "%(title)s.%(ext)s",
+            # "restrictfilenames": True, # replace space with _
+            "ignoreerrors": True,
+            "format": "bestaudio/best",
+            "continuedl": True,
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }]
+        }
+    else:
+        ydl_opts = {
+            "outtmpl": "%(title)s.%(ext)s",
+            # "restrictfilenames": True, # replace space with _
+            "ignoreerrors": True,
+            "format": "mp4",
+            "continuedl": True
+        }
+        
+    ydl = yt_dlp.YoutubeDL(ydl_opts)
+    
+    if not sys.stdin.isatty():
+        for line in sys.stdin:
+            if line == "\n": continue
 
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(urls)
+            url = line.strip()
+            ydl.download(url)
+    else:
+        while True:
+            url = input("\u001b[33;1m[URL]\u001b[0m ")
+            if url == "quit" or url == "q" or url == "exit" or url == "e": break
+            
+            ydl.download(url)
+
+if __name__ == '__main__':
+    main()
